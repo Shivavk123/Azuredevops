081e712f25e38ac0095206cd9e12d52d25c494f7
shivavk123 <shivavk123@gmail.com>
2023-01-21 15:30:25 +0530

    test76

--- main.tf
+++ main.tf
@@ -5,11 +5,23 @@ provider "azurerm" {
 
 terraform {
     backend "azurerm" {
-        resource_group_name  = "tf_rg_blobstore"
-        storage_account_name = "tfstorageshivavkjdhfjd"
+        resource_group_name  = "azurerm_resource_group.tf_test.name"
+        storage_account_name = "azurerm_storage_account.newstorage.name"
         container_name       = "tfstate"
         key                  = "terraform.tfstate"
     }
+}
+
+resource "azurerm_storage_account" "newstorage" {
+  name                     = "shivastorageac"
+  resource_group_name      = azurerm_resource_group.tf_test.name
+  location                 = azurerm_resource_group.tf_test.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
 }
 
 variable "imagebuild" {
